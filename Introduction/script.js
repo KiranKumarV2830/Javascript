3cbaf82874c735dbca471fe6292f4f062a576cec
Kiran Kumar V <kumarkiran760790@gmail.com>
2026-06-29 08:42:56 +0530

    Learning Javascript

---
+++ Introduction/script.js
@@ -0,0 +1,10 @@
+/*console.log("Kiran Kumar V"); 
+                                console.log is used for printing.
+console.log(`I like Pizza`);
+
+// Pop up alert
+window.alert(`This is an alert`);*/
+
+//To change the text of the element by using document.get 
+document.getElementById("myH1").textContent = `Kiran Kumar V`;
+document.getElementById("myP").textContent = `I am a student studying in dayanda sagar college of enginnerring doing ai/ml `
