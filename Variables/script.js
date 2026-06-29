0757f8bc5cbe2cde37bdbf5f350f915412b95769
Kiran Kumar V <kumarkiran760790@gmail.com>
2026-06-29 09:18:42 +0530

    Learning Javascript

---
+++ Variables/script.js
@@ -0,0 +1,38 @@
+// variable = It is container which stores a value
+//            Behaves as if it were the value it contains
+
+// 1 . declaration let x;
+// 2 . assignment = x=100;
+// We can do both declaration and assignment in single line of code
+// let x=123;
+
+// let age = 17 ;
+// let price = 10 ;    This is number data type
+// let cgpa = 9.1 ;
+
+// console.log(`You are ${age} years old and the price of the book you want is ${price} and the cgpa of yours is ${cgpa}`);
+
+// let firstName = "Kiran"; 
+// let FavFood = "Burger";               This is string data type
+// let email = "kumarkiran6789@gmail.com";
+
+// console.log(`Your name is ${firstName}.`); 
+// console.log(`Your favourite food is ${FavFood}.`);
+// console.log(`Your email is ${email}.`);
+
+// let online= true ;
+// let ForSale = true;                 This is Boolean data type
+// let isStudent = true;                
+   
+// console.log(typeof online);
+// console.log(`Bro is online: ${online}`);
+// console.log(`Is this car for sale : ${ForSale}`);
+// console.log(`Enrolled : ${isStudent}`);
+
+let FullName="Kiran Kumar V";
+let age = 17;
+let student = true;
+
+document.getElementById("p1").textContent = `Your name is ${FullName}`;
+document.getElementById("p2").textContent = `Your age is ${age}`;
+document.getElementById("p3").textContent = `are u a student ${student}`;
